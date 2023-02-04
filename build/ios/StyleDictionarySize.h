102820b19464724d16e208b94c5f9c47fe3a4167
isaotoyama <86751458+isaotoyama@users.noreply.github.com>
2023-02-04 08:06:29 -0800

    init

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Sat, 04 Feb 2023 15:54:52 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
